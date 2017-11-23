995f993f2221d0727c246e62d6d9fc77e9a9230f
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-23 20:05:01 -0200

    Aquivos de typescript.

---
+++ typescript/02-app.ts
@@ -0,0 +1,8 @@
+let mensagem: string = "Seja bem vindo!!!"
+let temporadaFriends: number = 10
+let estudandoAngular: boolean = true
+let notasDasProvas: Array<number> = [7.5, 8, 9]
+let notasDasProvas2: number [] = [7.5, 8, 9, 10]
+let listaDeFrutas: Array<string> = ['Uva', 'Banana', 'Abacaxi']
+let listaDeFrutas2: string [] = ['Banana', 'Abacaxi']
+
---
+++ typescript/dist/02-app.js
@@ -0,0 +1,8 @@
+"use strict";
+var mensagem = "Seja bem vindo!!!";
+var temporadaFriends = 10;
+var estudandoAngular = true;
+var notasDasProvas = [7.5, 8, 9];
+var notasDasProvas2 = [7.5, 8, 9, 10];
+var listaDeFrutas = ['Uva', 'Banana', 'Abacaxi'];
+var listaDeFrutas2 = ['Banana', 'Abacaxi'];
